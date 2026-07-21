c80c62479898d373f9dd94fd59263d57b2d7e9ca
daytwoat <sbroubde@gmail.com>
2026-07-21 23:22:06 +0100

    test save, fix reading empty file

--- src/blocklist.rs
+++ src/blocklist.rs
@@ -1,48 +1,48 @@
-use serde::{Serialize, Deserialize};
+use anyhow::{Result, bail};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
-use anyhow::{Result, bail};
 
-
-
-pub struct BlockedItem{
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct BlockedItem {
     pub term: String,
-    pub days: [u8; 7],
-    pub duration: u16, //in minutes
-    // pub from_to: [[String; 2]] //from to day. to implement later
 }
-
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Blocklist {
-    pub(crate) domains: Vec<String>, //testing with just strings
+    pub(crate) domains: Vec<BlockedItem>,
+    // pub days: [u8; 7],
+    // pub duration: u16,
 }
 
 impl Blocklist {
     pub fn new() -> Self {
-        Blocklist { domains: Vec::new() }
+        Blocklist {
+            domains: Vec::new(),
+        }
     }
 
     pub fn add(&mut self, domain: &str) -> Result<()> {
-        if self.domains.contains(&domain.to_string()) {
+        if self.domains.iter().any(|d| d.term == domain) {
             bail!("Domain already in blocklist");
         }
-        self.domains.push(domain.to_string());
+        self.domains.push(BlockedItem {
+            term: domain.to_string(),
+        });
         Ok(())
     }
 
     pub fn remove(&mut self, domain: &str) -> Result<()> {
-        if let Some(pos) = self.domains.iter().position(|d| d == domain) {
+        if let Some(pos) = self.domains.iter().position(|d| d.term == domain) {
             self.domains.remove(pos);
             Ok(())
         } else {
             bail!("Domain not found in blocklist");
-            Ok(())
         }
     }
 
     pub fn is_blocked(&self, domain: &str) -> bool {
-        self.domains.iter().any(|d| d == domain)
+        self.domains.iter().any(|d| d.term == domain)
     }
 
     pub fn save_to_file(&self, path: &str) -> Result<()> {
@@ -56,6 +56,10 @@ impl Blocklist {
             return Ok(Blocklist::new());
         }
         let data = fs::read_to_string(path)?;
+        if data.trim().is_empty() {
+            return Ok(Blocklist::new());
+        }
+
         let bl: Blocklist = serde_json::from_str(&data)?;
         Ok(bl)
     }
--- src/http_server.rs
+++ src/http_server.rs
@@ -1,56 +1,46 @@
-use axum::{
-    routing::post,
-    Router,
-    Json,
-    extract::State, 
-};
+use anyhow::Result;
+use axum::{Json, Router, extract::State, routing::post};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use anyhow::Result;
 use tokio::sync::RwLock;
-
-
 
 use crate::blocklist::Blocklist;
 
 const PORT: u16 = 8080;
 
 pub async fn run_http_server(state: Arc<RwLock<Blocklist>>) -> Result<()> {
-   let app = Router::new()
-    .route("/config", post(save_config).get(get_config)).with_state(state);
+    let app = Router::new()
+        .route("/config", post(save_config).get(get_config))
+        .with_state(state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
     println!("HTTP API running on http://{}", addr);
 
-    axum::serve(
-        tokio::net::TcpListener::bind(addr).await?,
-        app,
-    )
-    .await?;
+    axum::serve(tokio::net::TcpListener::bind(addr).await?, app).await?;
 
     Ok(())
 }
 
-
-async fn get_config() -> Json<Blocklist> {
+async fn get_config1() -> Json<Blocklist> {
     match std::fs::read_to_string("config.json") {
-        Ok(contents) => {
-            match serde_json::from_str::<Blocklist>(&contents) {
-                Ok(config) => Json(config),
-                Err(_) => Json(Blocklist { domains: vec![] }),
-            }
-        }
+        Ok(contents) if contents.trim().is_empty() => Json(Blocklist { domains: vec![] }),
+        Ok(contents) => match serde_json::from_str::<Blocklist>(&contents) {
+            Ok(config) => Json(config),
+            Err(_) => Json(Blocklist { domains: vec![] }),
+        },
         Err(_) => Json(Blocklist { domains: vec![] }),
     }
 }
 
+async fn get_config(State(state): State<Arc<RwLock<Blocklist>>>) -> Json<Blocklist> {
+    let config = state.read().await;
+    Json(config.clone())
+}
 
 async fn save_config(
     State(state): State<Arc<RwLock<Blocklist>>>,
     Json(payload): Json<Blocklist>,
 ) -> Json<&'static str> {
-
-
     //global meomory update
     {
         let mut config = state.write().await;
